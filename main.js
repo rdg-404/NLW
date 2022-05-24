0a78075e43ad0864ff62da069df908b77df8cb72
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-05-24 02:14:48 -0300

    add navegation point in menu itens using js

--- main.js
+++ main.js
@@ -4,7 +4,59 @@
  function onScroll() {
    showNavOnScroll()
    showBackToTopButtonOnScroll()
+
+   activateMenuAtCurrentSection(home)
+   activateMenuAtCurrentSection(services)
+   activateMenuAtCurrentSection(about)
+   // activateMenuAtCurrentSection(contact)
  }
+
+
+ function activateMenuAtCurrentSection(section){
+
+   const targetLine = scrollY + innerHeight / 2 
+
+
+   //verificar se a seção passou da linha
+   //Dados necessarios
+   const sectionTop = section.offsetTop 
+   const sectionHeight = section.offsetHeight 
+   const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+   // console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine)
+
+
+   //verificar se a base está abaixo da linha alvo
+   // dados necessarios
+   const sectionEndsAt = sectionTop + sectionHeight
+   const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+   // console.log('O fundo da seção passou da linha?', sectionEndPassedTargetLine)
+
+   const sectionBoundaries =
+      sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+
+   const sectionId = section.getAttribute('id') //pega o id da section como uma string
+   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`) //pega o href que contenha o id 
+
+   menuElement.classList.remove('active') //ja começa eliminando a class do elemento
+   if(sectionBoundaries){
+      menuElement.classList.add('active')
+   }
+
+
+
+ }
+
+
+
+
+
+
+
+
+
 
  function showNavOnScroll(){
    if(scrollY == 0){
